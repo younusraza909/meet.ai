42a0caea2ed02717d88d818902cc36ba6e687f2c
younusraza909 <younus@geeksofkolachi.com>
2025-06-14 22:48:32 +0500

    Add authentication functionality with better-auth integration
    - Implemented user authentication using better-auth, including sign-up and sign-in features.
    - Created API routes for authentication handling.
    - Updated database schema to support user, session, account, and verification tables.
    - Added auth client for managing authentication state in the application.
    - Updated package dependencies to include better-auth and related packages.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,75 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+
 export default function Home() {
-  return <div className="text-4xl text-green-500 font-bold">Hello World</div>;
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { data: session } = authClient.useSession();
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email,
+        name,
+        password,
+      },
+      {
+        onSuccess: () => {
+          window.alert("Success");
+        },
+        onError: () => {
+          window.alert("Something went wrong");
+        },
+      }
+    );
+  };
+  const onSignIn = () => {
+    authClient.signIn.email(
+      {
+        email,
+        password,
+      },
+      {
+        onSuccess: () => {
+          window.alert("Success");
+        },
+        onError: () => {
+          window.alert("Something went wrong");
+        },
+      }
+    );
+  };
+
+  if (session) {
+    return (
+      <div className="flex flex-col gap-y-4 p-4">
+        <p>Logged in as {session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}>Sing out</Button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-y-10">
+      <div className="p-4 flex flex-col gap-y-4 ">
+        <Input placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
+        <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Input placeholder="password" value={password} type="password" onChange={(e) => setPassword(e.target.value)} />
+
+        <Button onClick={onSubmit}>Create User</Button>
+      </div>
+      <div className="p-4 flex flex-col gap-y-4 ">
+        <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Input placeholder="password" value={password} type="password" onChange={(e) => setPassword(e.target.value)} />
+
+        <Button onClick={onSignIn}>Sign In</Button>
+      </div>
+    </div>
+  );
 }
